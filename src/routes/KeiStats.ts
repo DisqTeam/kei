56a0b64adf76eabbc22b553098589359d5e26f3c
etstringy <eddyteddyt@gmail.com>
2021-01-03 12:41:41 +0000

    AAA

--- src/routes/KeiStats.ts
+++ src/routes/KeiStats.ts
@@ -3,7 +3,7 @@ import Audios from '../db/models/AudioModel';
 import Deleted from '../db/models/DeletedModel'; 
 
 export default async (req: express.Request, res: express.Response): Promise<Response> => {
-    const count = await Audios.count()
+    const count = await Audios.countDocuments()
     const deleted = await Deleted.count()
     return res.status(200).json({ success: true, count, deleted })
 }
