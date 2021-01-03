9e4b3c062d834b1ccc07d61ef4d4067d4398442b
etstringy <eddyteddyt@gmail.com>
2021-01-03 08:15:23 +0000

    oops

--- src/routes/KeiStats.ts
+++ src/routes/KeiStats.ts
@@ -3,8 +3,6 @@ import Audios from '../db/models/AudioModel';
 import Deleted from '../db/models/DeletedModel'; 
 
 export default async (req: express.Request, res: express.Response): Promise<Response> => {
-    if(!req.query.id) return res.status(400).send({ success: false, message: "No audio ID specified" })
-
     const count = await Audios.count()
     const deleted = await Deleted.count()
     return res.status(200).json({ success: true, count, deleted })
